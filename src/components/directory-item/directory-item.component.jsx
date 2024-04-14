a8719a0e3fac2c319590afffcf912dfa00268e38
Blake Mullinax <blakemullinax@charter.net>
2024-04-14 14:30:42 -0500

    Add links to directory componenet

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-    const { title, imageUrl } = category;
+    const { title, imageUrl, route } = category;
+
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
 
     return (
-        <DirectoryItemContainer>
-            <BackgroundImage imageUrl={imageUrl} />
+        <DirectoryItemContainer onClick={onNavigateHandler}>
+            <BackgroundImage imageurl={imageUrl} />
             <Body>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
